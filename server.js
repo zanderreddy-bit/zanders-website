6ed286b633f95a77762c198bbae0b17faaa5d108
ZANDERREDDY <zanderreddy@gmail.com>
2026-09-25 11:49:56 -0500

    add pic

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get('/api/getName', (req, res) => {
                 });
  
 
-//app.get('/api/getImage', (req, res) => {
-           //         res.set('Access-Control-Allow-Origin', '*');
-               //     res.sendFile(__dirname ,'IMG_4799.jpg');
-               // });
+app.get('/api/getImage', (req, res) => {
+                    res.set('Access-Control-Allow-Origin', '*');
+                    res.sendFile(__dirname ,'IMG_4799.jpg');
+                });
                 
 
                 
